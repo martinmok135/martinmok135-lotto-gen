bc1424de7534c09a1b046d4dedbce5c61a931a27
martin.mok <martin.mok@meth-encore.com>
2025-03-19 10:37:58 +0900

    주석 수정(master)

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function generate() {
 	return numbers;
 }
 
+//== 배열 안에 number가 포함되어 있는지 확인 ==//
 function contains(numbers, number) {
 		for(var i=o; i<numbers.length; i++) {
 			if(numbers[i] == number) return true;
