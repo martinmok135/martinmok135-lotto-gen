1fe3617b125286cab2abc398dd31aaba737d274c
martin.mok <martin.mok@meth-encore.com>
2025-03-19 10:35:02 +0900

    버그 수정 - 중복된 번호 문제

--- js/app.js
+++ js/app.js
@@ -2,8 +2,22 @@ function generate() {
 	var numbers = [];
 
 	for(var i=0; i<6; i++) {
-		numbers.push(Math.floor(Math.random() * 45 )+1);
-	}
+		var number = Math.floor(Math.random() * 45 )+1;
+	
+		if(contaubs(numbers, number)) {
+			i--;
+		}	else {
+			numbers.push(number);
+		}
+	{
 
 	return numbers;
 }
+
+function contains(numbers, number) {
+		for(var i=o; i<numbers.length; i++) {
+			if(numbers[i] == number) return true;
+		}
+		return false;
+}
+
